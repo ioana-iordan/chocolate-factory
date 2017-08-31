e10d570bddce0ce765b0863a2dbd7ff70e26e1b3
Horia Radu <horia.radu23@gmail.com>
2017-08-31 14:55:14 +0300

    Update test.js

--- src/test.js
+++ src/test.js
@@ -1,6 +1,6 @@
 let marmota = new Marmota();
 { manca(); let iarba_verde = 10; }
-let ciocolate = getCiocolate();
+var ciocolate = getCiocolate();
 
 function traba() {
   for (var ciocolata in ciocolate) {
