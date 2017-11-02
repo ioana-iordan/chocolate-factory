4ef78846bdbeba792b0c62915b9766479ddcdd89
kale-io <alpha@codeyourkale.ai>
2017-11-02 15:06:56 +0200

    test

--- src/test.js
+++ src/test.js
@@ -1,17 +1,1 @@
-var ciocolate;
-let marmota = new Marmota();
-{ manca(); let iarba_verde = 10; }
-ciocolate = getCiocolate();
-
-
-
-function traba() {
-  for (var ciocolata in ciocolate) {
-    marmota.inveleste(ciocolata, "staniol")
-  }
-  fericita(marmota);
-}
-
-if (2 == 3) {}
-if (2 === 4) {}
-if (2 === 5) {} else {}
+Zm9vb2JhcmJheg==
