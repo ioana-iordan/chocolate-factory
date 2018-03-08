60fbfcb3b610d174c993c2be6d5732ccef91eb67
Horia Radu <horia.radu23@gmail.com>
2018-03-08 11:54:33 +0200

    Update test.js

--- src/test.js
+++ src/test.js
@@ -1,4 +1,19 @@
-if (1 === 1) {}
-if (2 === 123) {}
-if (3 === 3) {} else {}
-if (4 === 5) {} else {}
+if (1 == 1) {}
+if (2 == 123) {}
+if (3 == 3) {} else {}
+if (4 == 5) {} else {}
+
+function dummt() {
+  if (5 === -0) {
+    console.log("x");
+  }
+  if (a == b) {
+    console.log(a);
+  }
+  try {
+    1 + 1;
+  } catch (e) {
+    console.log("don't remove me: ", e);
+  }
+  console.log("i will be gone");
+}
